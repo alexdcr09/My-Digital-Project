252e019bbaf3111bc26c712aeb0821a5d4a035bd
Alexandre Ducarre <alex@MacBook-Air-de-Alexandre.local>
2021-05-12 14:16:00 +0200

    change server.js file

---
+++ back/app/config/db.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    HOST: 'postgres://yrvmtizmzprjob:fc625d41b919cd2c099a0581a59d969d0bc7ce3073af00a94f1e07ac81171b5d@ec2-54-163-254-204.compute-1.amazonaws.com:5432/dc76ng6ooc40t1',
+    USER: 'root',
+    PASSWORD: 'root',
+    DATABASE: 'MyDigitalProject'
+  };
